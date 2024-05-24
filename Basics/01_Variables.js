de153b78207d21294c9124c9549261cee89e3859
Debesh Mondal <debeshprofessional05@gmail.com>
2024-05-24 09:21:21 +0530

    new

---
+++ Basics/01_Variables.js
@@ -0,0 +1,21 @@
+const accountId =144134
+let accountEmail = "debesh@google.com"
+var accountPassword = "122340"
+accountcity = "Delhi"
+
+// accountId = 2 ----> Not allowed
+// console.log(accountId);
+
+accountEmail = "deb@gmai.com"
+// console.log(accountEmail);
+
+accountPassword = "34567"
+// console.log(accountPassword);
+
+accountcity = "Goa"
+// console.log(accountcity);
+
+
+console.table([ accountId, accountEmail, accountPassword, accountcity ])
+
+console.table({ accountId, accountEmail, accountPassword, accountcity })
---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,12 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert("H")  alert will not work as we r using node now to browser
+
+console.log(3+3);
+
+
+let name ="Deb"
+let age //= 44
+
+let isLoggedIn = true;
+// console.log(age);
